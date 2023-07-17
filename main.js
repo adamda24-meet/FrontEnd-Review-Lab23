9870ffcfe5a46ae412dd7cbc4d8e9cf4442e8884
adamda24-meet <adamdarawshe380@gmail.com>
2023-07-17 14:07:49 +0300

    coollllll

---
+++ main.js
@@ -0,0 +1,9 @@
+//function changeBackgroundColor(color) {
+//	document.getElementsById("bo").style.backgroundColor="color";
+//}
+//let changeBackgroundColor("red");
+function changeBackground(color) {
+   document.body.style.background = color;
+}
+
+//window.addEventListener("load",function() { changeBackground('red') });
