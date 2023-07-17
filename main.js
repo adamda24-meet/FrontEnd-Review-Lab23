96ef68bbda156fd9acc851b10d1ae1296657aa9a
adamda24-meet <adamdarawshe380@gmail.com>
2023-07-17 14:18:19 +0300

    this is the bonussss(:

--- main.js
+++ main.js
@@ -2,8 +2,13 @@
 //	document.getElementsById("bo").style.backgroundColor="color";
 //}
 //let changeBackgroundColor("red");
-function changeBackground(color) {
-   document.body.style.background = color;
+function changeBackground(color1,color2) {
+	var n =document.body.style.background
+	if(n==color1){
+   document.body.style.background = color2;}
+   else{
+   	document.body.style.background = color1;}
 }
+
 
 //window.addEventListener("load",function() { changeBackground('red') });
